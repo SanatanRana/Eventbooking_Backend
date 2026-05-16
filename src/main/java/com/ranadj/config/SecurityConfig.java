5e39326b4a2b7abf14fffc286bacbbae0a530151
sanatan rana <sanatanr41@gmail.com>
2026-05-16 23:46:34 +0530

    fixed

--- src/main/java/com/ranadj/config/SecurityConfig.java
+++ src/main/java/com/ranadj/config/SecurityConfig.java
@@ -72,8 +72,8 @@ public class SecurityConfig {
     @Bean
     public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
         org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
-        configuration.setAllowedOrigins(java.util.List.of("http://localhost:5173"));
-        configuration.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedOrigins(java.util.List.of("http://localhost:5173", "https://djsanatan.sanatanrana.me", "https://www.djsanatan.sanatanrana.me"));
+        configuration.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "*"));
         configuration.setAllowedHeaders(java.util.List.of("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true);
         
